484cdf2d2146a49aec4a2b9e593a1333f6344850
GotPop <gotpop@users.noreply.github.com>
2023-02-10 15:22:59 +0000

    `Feature:` Updating pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"
+
+import { Bebas_Neue, Montserrat } from "@next/font/google"
+
+import type { AppProps } from "next/app"
+
+const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-bebas",
+})
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-montserrat",
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div className={`${bebas.variable} ${montserrat.variable}`}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  )
 }
