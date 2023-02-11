3ffd504b7fbf6ffe67c52c628736ecb429c24d9d
GotPop <gotpop@users.noreply.github.com>
2023-02-11 14:25:26 +0000

    `Feature:` Adding `Tailwind` custom props

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,14 +5,31 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    fontFamily: {
-      heading: ["var(--font-bebas)"],
-      body: ["var(--font-montserrat)"],
-    },
-    colors: {
-      'primary': '#FBBD5A',
-      'accent': '#c2410c',
-    },
+    extend: {
+      fontFamily: {
+        heading: ["var(--font-bebas)"],
+        body: ["var(--font-montserrat)"],
+      },
+      colors: {
+        'primary': '#FBBD5A',
+        'secondary': '#EBA639',
+        'accent': '#c2410c',
+        'brand-grey-1': '#EBEBEB',
+        'brand-grey-2': '#333131',
+        'brand-grey-3': '#161616',
+        'brand-grey-4': '#BFBFBF',
+        'brand-grey-5': '#292828',
+        'brand-grey-6': '#1C1C1C',
+        'brand-grey-7': '#2B2929',
+      },
+      backgroundImage: {
+        'hero': "url('../../public/images/bg.jpg')",
+        'glow': "url('../../public/images/glow.png')",
+      },
+      scale: {
+        '99': '.99',
+      }
+    }
   },
   plugins: [],
 }
